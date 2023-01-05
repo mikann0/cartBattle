3ffc6e24dc262c262982b4c7727aa812af7c7ba2
William <tgp.william@gmail.com>
2023-01-05 15:51:45 +0100

    test storage local

--- cardBattle.js
+++ cardBattle.js
@@ -43,39 +43,56 @@ let cardname = document.querySelectorAll('.cardNameTxt');
 let damageNumber = document.querySelectorAll('.damage');
 let healNumber = document.querySelectorAll('.heal');
 
-// for (var i = 0; i < listCard.length; i++){
-//     console.log(i)
-//     console.log(listCard[i])
-//     chooseCard[i].addEventListener('click', function(){
-//         console.log(i)
-//         showCard(i)
-//     })
-// }
-
-chooseCard[0].addEventListener('click', function(){
-    showCard(0)})
-chooseCard[1].addEventListener('click', function(){
-    showCard(1)})
-chooseCard[2].addEventListener('click', function(){
-    showCard(2)})
-chooseCard[3].addEventListener('click', function(){
-    showCard(3)})
-chooseCard[4].addEventListener('click', function(){
-    showCard(4)})
-chooseCard[5].addEventListener('click', function(){
-    showCard(5)})
+for (var i = 0; i < listCard.length; i++){
+    console.log(i)
+    console.log(listCard[i])
+    chooseCard[i].addEventListener('click', function(){
+        console.log(i)
+        showCard(i)
+    })
+}
 
 function showCard(i){
     console.log(listCard[i])
 }
 
+
+
+
+
+
+
+
+
+// chooseCard[0].addEventListener('click', function(){
+//     showCard(0)})
+// chooseCard[1].addEventListener('click', function(){
+//     showCard(1)})
+// chooseCard[2].addEventListener('click', function(){
+//     showCard(2)})
+// chooseCard[3].addEventListener('click', function(){
+//     showCard(3)})
+// chooseCard[4].addEventListener('click', function(){
+//     showCard(4)})
+// chooseCard[5].addEventListener('click', function(){
+//     showCard(5)})
+
+
+
+
+
+
 for (let i = 0; i < listCard.length; i++) {
     damageNumber[i].textContent += listCard[i].attack   
-}
-for (let i = 0; i < listCard.length; i++) {
     healNumber[i].textContent += listCard[i].heal   
-}
-for (let i = 0; i < listCard.length; i++) {
     cardname[i].textContent += listCard[i].name   
 }
 
+// let a = 12
+
+
+// localStorage.setItem("cardChoisie",a);
+
+// let cat = localStorage.getItem(a);
+
+// console.log(cat)
